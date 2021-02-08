edb5c835665a6179c802907028bf0389a703e54c
prudhvi2020 <59828263+prudhvi2020@users.noreply.github.com>
2021-02-08 17:17:07 +0530

    Delete s1.sh

--- s1.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-freememory=$(free -mt | grep -w "Total" | awk '{print $4}')
-cpu_usage=$(top -b -n 2 -d1 | grep "Cpu(s)" | tail -n1 | awk '{print $2}' | cut -d "." -f 1)
-if [ $cpu_usage -ge 0 -a $freememory -ge 30 ]; then
-echo "The cpu-usage is :$cpu_usage"
-echo "The freememory is : $freememory"
-else
- echo "the system is good"
-fi
